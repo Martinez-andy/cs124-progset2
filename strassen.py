c2940236d703f2f62fc4bccbdef00ce76aa5e98e
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2024-03-27 06:34:17 -0400

    First attempt at strasens

--- strassen.py
+++ strassen.py
@@ -40,7 +40,24 @@ def matMult(m1, m2):
     return result
 
 
+# Take a matrix as input and return 4 quadrants
+def makeQuads(m):
+    # Get the dimensions of the matrix
+    n = m.shape[0]
 
+    # Calculate splitting points
+    mid_row = n // 2
+    mid_col = n // 2
+    
+    uLeft = m[:mid_row, :mid_col]
+    uRight = m[:mid_row, mid_col:]
+    lLeft = m[mid_row:, :mid_col]
+    lRight = m[mid_row:, mid_col:]
+
+    return uLeft, uRight, lLeft, lRight
+
+
+# Strasen's algorithm
 def strasens(m1, m2):
     n = len(m1)
     
@@ -48,7 +65,7 @@ def strasens(m1, m2):
     if n <= nOpt:
         return matMult(m1, m2)
     
-    # Handle odd case by adding col and row of zeros
+    # Handle odd matrix case
     if n % 2:
         m1 = addZeros(m1)
         m2 = addZeros(m2)
@@ -56,9 +73,26 @@ def strasens(m1, m2):
     
     # Otherwise, strasens algorithm
     
-    # TODO: Implement algorithm
+    A, B, C, D = makeQuads(m1)
+    E, F, G, H = makeQuads(m2)
     
-    return None
+    p1 = strasens(A, (F-H))
+    p2 = strasens((A + B), H)
+    p3 = strasens((C + B), E)
+    p4 = strasens(D, (G - E))
+    p5 = strasens((A + D), (E + H))
+    p6 = strasens((B - D), (G + H))
+    p7 = strasens((C - A), (E + F))
+    
+    return np.vstack(
+        np.hstack(
+        -p2 + p4 + p5 + p6,
+        p1 + p2
+    ), 
+        botRes = np.hstack(
+        p3 + p4,
+        p1 - p3 + p5 + p7
+    ))
 
 
 
