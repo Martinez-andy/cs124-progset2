8f77a8422eff943d396a95aad2d9b6a864365a23
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2024-03-28 18:48:00 -0400

    This version passes autograder

--- test.py
+++ test.py
@@ -2,15 +2,5 @@
 import sys
 from strassen import main  # Import your strassen.py module
 
-def test():
-    file = "strassen.py"
-
-    # Set sys.argv with script name, flag, dimension, and filename
-    sys.argv = [file, "0", "2", "test1.txt"]
-    main()
-
-    # Set sys.argv for the second test
-    sys.argv = [file, "0", "3", "test2.txt"]
-    main()
-
-test()
+def findOptN():
+    sys.argv = ["strassen.py", "0", "2", "test1.txt"]
