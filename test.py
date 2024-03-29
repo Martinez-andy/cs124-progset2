116330c250ca8ba1eb0e945c41be747638b546c0
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2024-03-28 20:54:45 -0400

    Added test files

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 # test_strasen.py
 import sys
-  # Import your strassen.py module
 
 def main():
-    with open("test7.txt", "w") as file:
-        for _ in range(2):
-            for i in range(1, 101):
-                file.write(f"{i}\n")
-                
+    sizes = [2, 4, 8, 16, 32, 64, 128, 256]
+    
+    for i, d in enumerate(sizes):
+        with open(f"test{i}.txt", "w") as file:
+            for _ in range(2):
+                for j in range(1, (d ** 2) + 1):
+                    file.write(f"{j}\n")
+                    
 main()
