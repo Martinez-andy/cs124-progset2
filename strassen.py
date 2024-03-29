32bf4e68a6509a97d252fe3e009ed89d59e34460
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2024-03-28 20:58:19 -0400

    Moved part 3 into a different file

--- strassen.py
+++ strassen.py
@@ -11,7 +11,6 @@ import sys
 nOpt = 37
 
 
-
 # Adds row and column of zeros, needed for odd matrices
 def addZeros(m):
 
@@ -130,38 +129,6 @@ def main():
     for i in range(d):
         print(int(m[i, i]))
             
-main()
-# Part 3 - TBD
-# independent variable
-p_list = [0.01, 0.02, 0.03, 0.04, 0.05]
-# results from our algorithm 
-triangle_nums = []
-# expected number of triangles
-comparison_nums = []
-n = 1024
 
-for p in p_list:
-    tri_avg = 0
-    # find the avg of 10 trials for every value of p
-    for _ in range(10):
-        a = np.zeros(n,n)
-        for i in range(n):
-            for j in range(n):
-                if i >= j:
-                    a[i][j] = np.binomial(p)
-                else:
-                    a[i][j] = a[j][i]   
-                    
-        a2 = strassens(a,a)
-        a3 = strassens(a2,a)
-        trisum = 0
-        for j in range(n):
-            trisum += a[j][j]
-        trisum = trisum / 6
-        tri_avg += trisum
-    tri_avg = tri_avg / 10
-    triangle_nums.append(tri_avg)
-    comparison = 1024*1023*1022/6
-    comparison = comparison * (p**3)
-    comparison_nums.append()
-# TODO: plot
+
+# part 3 moved to part3.py
--- test.py
+++ test.py
@@ -1,13 +1,8 @@
 # test_strasen.py
 import sys
+import strassen
 
 def main():
-    sizes = [2, 4, 8, 16, 32, 64, 128, 256]
     
-    for i, d in enumerate(sizes):
-        with open(f"test{i}.txt", "w") as file:
-            for _ in range(2):
-                for j in range(1, (d ** 2) + 1):
-                    file.write(f"{j}\n")
                     
 main()
